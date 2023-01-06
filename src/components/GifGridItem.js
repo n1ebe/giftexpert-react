a27c6e7201d19813301884a7a2fc6fd995870edf
Ignacio Parodi <sweetcherryapps@gmail.com>
2023-01-06 09:30:48 -0300

    actualizadop

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ( { title, url} ) => {
 
   return (
     <div className="card animate__animated animate__bounce">
-        <img src={ url } alt={ title } />
+        <img src = { url } alt = { title } />
         <p> { title } </p>
     </div>
 )
